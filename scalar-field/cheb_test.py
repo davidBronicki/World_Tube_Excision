eeed14832c3cab3a121571de85bd5047272fdd66
David Bronicki <dbronick@go.olemiss.edu>
2022-04-13 00:24:24 -0500

    solving things now

--- scalar-field/cheb_test.py
+++ scalar-field/cheb_test.py
@@ -1,7 +1,10 @@
 import numpy as np
+from numpy import ndarray
 import scipy
 import matplotlib.pyplot as plt
 from typing import Callable
+
+from sympy import chebyshevt
 
 class ChebEngine:
 	def __init__(self, order: int):
@@ -43,13 +46,14 @@ class ModalRepresentation:
 	def __init__(self, chebEngine: ChebEngine, data: np.ndarray):
 		self.chebEngine = chebEngine
 		self.data = data
-		self.order = len(data) // 2
 	
 	def __mul__(self, other):
 		if type(other) == ModalRepresentation:
 			return ModalRepresentation(self.chebEngine, self.data * other.data)
 		else:
 			return ModalRepresentation(self.chebEngine, self.data * other)
+	
+	__rmul__ = __mul__
 	
 	def __div__(self, scalar):
 		return ModalRepresentation(self.chebEngine, self.data / scalar)
@@ -63,8 +67,7 @@ class ModalRepresentation:
 class SpectralRepresentation:
 	def __init__(self, chebEngine: ChebEngine, data: np.ndarray):
 		self.chebEngine = chebEngine
-		self.data = np.array(data)
-		self.order = len(data) // 2
+		self.data = data
 	
 	def __mul__(self, scalar):
 		return SpectralRepresentation(self.chebEngine, self.data * scalar)
@@ -79,26 +82,25 @@ class SpectralRepresentation:
 		return SpectralRepresentation(self.chebEngine, self.data - other.data)
 
 def functionToModal(funct: Callable[[float],float], chebEngine: ChebEngine, x0 = -1.0, x1 = 1.0) -> ModalRepresentation:
-	data = np.zeros(2 * chebEngine.order)
+	data = np.zeros(chebEngine.N + 1)
 	data[0] = funct(chebEngine.getCollocationPoint(0, x0, x1))
 	data[chebEngine.N] = funct(chebEngine.getCollocationPoint(chebEngine.N, x0, x1))
 	for i in range(1, chebEngine.N):
 		data[i] = funct(chebEngine.getCollocationPoint(i, x0, x1))
-		data[2 * chebEngine.N - i] = data[i]
 	return ModalRepresentation(chebEngine, data)
 
-def modalToSpectral(modalRep: ModalRepresentation) -> ModalRepresentation:
+def modalToSpectral(modalRep: ModalRepresentation) -> SpectralRepresentation:
 	N = modalRep.chebEngine.N
-	specData = np.fft.rfft(modalRep.data) / N
+	specData = scipy.fft.dct(modalRep.data, type=1) / N
 	specData[0] /= 2
 	specData[N] /= 2
 	return SpectralRepresentation(modalRep.chebEngine, specData)
 
-def spectralToModal(specRep: SpectralRepresentation) -> SpectralRepresentation:
+def spectralToModal(specRep: SpectralRepresentation) -> ModalRepresentation:
 	N = specRep.chebEngine.N
 	specRep.data[0] *= 2
 	specRep.data[N] *= 2
-	modalData = np.fft.irfft(specRep.data) * N
+	modalData = scipy.fft.dct(specRep.data, type=1) / 2
 	specRep.data[0] /= 2
 	specRep.data[N] /= 2
 	return ModalRepresentation(specRep.chebEngine, modalData)
@@ -114,85 +116,129 @@ def spectralToFunction(specRep: SpectralRepresentation, x0 = -1.0, x1 = 1.0) -> 
 def spectralDerivative(specRep: SpectralRepresentation, domainWidth = 2.0) -> SpectralRepresentation:
 	N = specRep.chebEngine.N
 	widthCorrectionFactor = 2.0 / domainWidth
-	outputData = np.zeros(N + 1, dtype=complex)
+	outputData = np.zeros(N + 1)
 	outputData[N - 1] = 2 * N * specRep.data[N]
 	outputData[N - 2] = 2 * (N - 1) * specRep.data[N - 1]
 	for i in reversed(range(0, N - 2)):
-		outputData[i] = outputData[i + 2] + (i + 1) * specRep.data[i + 1]
+		outputData[i] = outputData[i + 2] + 2 * (i + 1) * specRep.data[i + 1]
 	outputData[0] /= 2
 	return SpectralRepresentation(specRep.chebEngine, outputData * widthCorrectionFactor)
 
 def modalBasisFunction(chebEngine: ChebEngine, n: int) -> ModalRepresentation:
-	outputData = np.zeros(chebEngine.N * 2)
-	if n == 0:
-		outputData[0] = 1
-	else:
-		outputData[n] = 1
-		outputData[2 * chebEngine.N - n] = 1
+	outputData = np.zeros(chebEngine.N + 1)
+	outputData[n] = 1
 	return ModalRepresentation(chebEngine, outputData)
 
 def spectralBasisFunction(chebEngine: ChebEngine, n: int) -> SpectralRepresentation:
-	outputData = np.zeros(chebEngine.N * 2)
+	outputData = np.zeros(chebEngine.N + 1)
 	outputData[n] = 1
-	# if n == 0:
-	# 	outputData[0] = 1
-	# else:
-	# 	outputData[n] = 1
-	# 	outputData[2 * chebEngine.N - n] = 1
 	return SpectralRepresentation(chebEngine, outputData)
 
-def promoteModalData(chebEngine: ChebEngine, data: np.ndarray) -> ModalRepresentation:
-	outputData = np.zeros(chebEngine.N * 2)
-	outputData[0] = data[0]
-	for i in range(1, chebEngine.N + 1):
-		outputData[i] = data[i]
-		outputData[2 * chebEngine.N - i] = data[i]
-	return ModalRepresentation(chebEngine, outputData)
-def promoteSpectralData(chebEngine: ChebEngine, data: np.ndarray) -> SpectralRepresentation:
-	outputData = data
-	# outputData = np.zeros(chebEngine.N * 2)
-	# outputData[0] = data[0]
-	# for i in range(1, chebEngine.N + 1):
-	# 	outputData[i] = data[i]
-	# 	outputData[2 * chebEngine.N - i] = data[i]
-	return SpectralRepresentation(chebEngine, outputData)
+def modalMultiplicationOperator(modalRep: ModalRepresentation) -> np.ndarray:
+	return np.diag(modalRep.data)
 
-def demoteSpectralData(spectralRep: SpectralRepresentation) -> np.ndarray:
-	return spectralRep.data.real
-	# outputData = np.zeros(len(spectralRep.data) // 2 + 1)
-	# for i in range(len(outputData)):
-	# 	outputData[i] = spectralRep.data[i]
-	# return outputData
-def demoteModalData(modalRep: ModalRepresentation) -> np.ndarray:
-	outputData = np.zeros(len(modalRep.data) // 2 + 1)
-	for i in range(len(outputData)):
-		outputData[i] = modalRep.data[i]
-	return outputData
+def modalDifferentialOperator(chebEngine: ChebEngine) -> np.ndarray:
+	N = chebEngine.N
+	operatorMatrix = np.zeros((N + 1, N + 1))
+	for i in range(N+1):
+		modalUnit = modalBasisFunction(chebEngine, i)
+		modalDeriv = spectralToModal(spectralDerivative(modalToSpectral(modalUnit)))
+		operatorMatrix[i] = modalDeriv.data
 
+	operatorMatrix = np.transpose(operatorMatrix)
+	return operatorMatrix
 
-def linearSolveTest(N):
+# def harmonicSolveTest(N: int, omega = 10, b = 3):
+# 	chebEngine = ChebEngine(N)
+
+def nonLinearSolveTest(N: int, a = 1.0):
+	#trying to solve (y')^2=a y, y(0)=1  -->  y(x)=x^2/4+ax+a^2
+	chebEngine = ChebEngine(N)
+
+	# estimatedSolution = spectralToModal(spectralBasisFunction(chebEngine, 1))
+	estimatedSolution = functionToModal(lambda x: x/2 + 1, chebEngine)
+
+	errors = []
+	def calcError():
+		estimatedSolutionPrime = spectralToModal(
+			spectralDerivative(modalToSpectral(estimatedSolution)))
+		errorFunction = estimatedSolutionPrime * estimatedSolutionPrime - estimatedSolution
+		errorFunction *= errorFunction
+		return sum(errorFunction.data)
+	
+	errors.append(calcError())
+	while(errors[-1] > 1e-12):
+		linearMatrix = np.zeros((N+1, N+1))
+		boundaryVector = np.zeros(N+1)
+
+		estimatedSolutionPrime = spectralToModal(
+			spectralDerivative(modalToSpectral(estimatedSolution)))
+		sourceVector = (a * estimatedSolution - estimatedSolutionPrime * estimatedSolutionPrime).data
+		sourceVector[N] = 0
+
+		for n in range(N+1):
+			modalUnit = modalBasisFunction(chebEngine, n)
+			specVector = modalToSpectral(modalUnit)
+
+			columnVector = spectralToModal(spectralDerivative(specVector))
+			columnVector *= 2 * estimatedSolutionPrime
+			columnVector -= a * modalUnit
+			linearMatrix[n] = columnVector.data
+			boundaryVector[n] = spectralToFunction(specVector)(0)
+		
+		linearMatrix = np.transpose(linearMatrix)
+		linearMatrix[N] = boundaryVector
+
+		solutionMatrix = np.linalg.inv(linearMatrix)
+		estimatedSolution += ModalRepresentation(
+			estimatedSolution.chebEngine, solutionMatrix.dot(sourceVector))
+		
+		errors.append(calcError())
+	
+	plt.plot(list(range(len(errors))), errors)
+	plt.yscale('log')
+	plt.show()
+	
+	estimatedFunction = spectralToFunction(modalToSpectral(estimatedSolution))
+	def analyticFunction(x):
+		return x**2 / 4 + a * x + a**2
+
+	xList = np.arange(-1.0, 1.0, 0.01)
+	yList = np.zeros(len(xList))
+	solutionList = np.zeros(len(xList))
+	for i in range(len(xList)):
+		yList[i] = analyticFunction(xList[i])
+		solutionList[i] = estimatedFunction(xList[i])
+	
+	plt.plot(xList, yList)
+	plt.plot(xList, solutionList)
+	plt.show()
+
+def linearSolveTest(N, a=5):
+	#trying to solve y'=a xy, y(0)=1  -->  y(x)=e^(-a x^2/2)
 	chebEngine = ChebEngine(N)
 	operatorMatrix = np.zeros((N+1, N+1))
+	boundaryVector = np.zeros(N+1)
+
 	sourceVector = np.zeros(N+1)
 	sourceVector[N] = 1
 
 	for i in range(N+1):
 		modalUnit = modalBasisFunction(chebEngine, i)
-		print(len(modalUnit.data))
 		specVector = modalToSpectral(modalUnit)
-		print(len(specVector.data))
 		specDeriv = spectralDerivative(specVector)
-		print(len(specDeriv.data))
 		modalDeriv = spectralToModal(specDeriv)
-		print(len(modalDeriv.data))
 		unitFunct = spectralToFunction(specVector)
-		if i != N:
-			operatorMatrix[i] = demoteModalData(modalUnit - modalDeriv)
-		operatorMatrix[N][i] = unitFunct(0)
+		operatorMatrix[i] = (a * modalUnit * chebEngine.getCollocationPoint(i) + modalDeriv).data
+
+		boundaryVector[i] = unitFunct(0)
+
+	operatorMatrix = np.transpose(operatorMatrix)
+	operatorMatrix[N] = boundaryVector
 
 	solutionMatrix = np.linalg.inv(operatorMatrix)
 
-	modalSolution = promoteModalData(chebEngine, solutionMatrix.dot(sourceVector))
+	modalSolution = ModalRepresentation(chebEngine, solutionMatrix.dot(sourceVector))
 	spectralSolution = modalToSpectral(modalSolution)
 	functionSolution = spectralToFunction(spectralSolution)
 
@@ -200,15 +246,12 @@ def linearSolveTest(N):
 	yList = np.zeros(len(xList))
 	solutionList = np.zeros(len(xList))
 	for i in range(len(xList)):
-		yList[i] = np.exp(xList[i])
+		yList[i] = np.exp(-a * xList[i]**2 / 2)
 		solutionList[i] = functionSolution(xList[i])
 	
 	plt.plot(xList, yList)
 	plt.plot(xList, solutionList)
 	plt.show()
-
-linearSolveTest(10)
-
 
 def basicTest(N, a = 5.0):
 	def exampleFunction(x: float) -> float:
@@ -217,25 +260,11 @@ def basicTest(N, a = 5.0):
 		y = exampleFunction(x)
 		return a * y * (1 - y)
 
-
-	# def exampleFunction(x: float) -> float:
-	# 	return np.sin(0.5 * x) - 0.5*np.cos(1.7 * x)+ 0.2 * np.sin(7.0 * x + 2)
-	# def exampleDerivative(x: float) -> float:
-	# 	return 0.5 * np.cos(0.5 * x) + 0.5 * 1.7 * np.sin(1.7 * x) + 0.2 * 7.0 * np.cos(7.0 * x + 2)
-
-
-	# def exampleFunction(x):
-	# 	return 1 if x > 0 else 0
-
-
-	# def exampleFunction(x):
-	# 	return np.abs(x)
-	# def exampleDerivative(x):
-	# 	return 1 if x > 0 else -1
-
 	chebEngine = ChebEngine(N)
 
 	modalRep = functionToModal(exampleFunction, chebEngine)
+	specRep = modalToSpectral(modalRep)
+	modalRep = spectralToModal(specRep)
 	specRep = modalToSpectral(modalRep)
 	approxFunct = spectralToFunction(specRep)
 
@@ -250,12 +279,9 @@ def basicTest(N, a = 5.0):
 	approxYList = np.zeros(len(xList))
 	for i in range(len(xList)):
 		approxYList[i] = approxFunct(xList[i])
-		# for n in range(N + 1):
-		# 	approxYList[i] += chebEval(xList[i], n) * coefficients[n]
 
 	plt.plot(xList, yList)
 	plt.plot(xList, approxYList)
-	# plt.plot(xList, approxYList - yList)
 	plt.show()
 
 	for i in range(len(xList)):
@@ -263,7 +289,9 @@ def basicTest(N, a = 5.0):
 		approxYList[i] = approxDeriv(xList[i])
 	plt.plot(xList, yList)
 	plt.plot(xList, approxYList)
-	# plt.plot(xList, approxYList - yList)
 	plt.show()
 
-basicTest(10)
+
+nonLinearSolveTest(10)
+# linearSolveTest(10)
+# basicTest(10)
